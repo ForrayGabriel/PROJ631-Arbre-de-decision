36bfee083114f9766954d0a4e2751de625570a1b
Gabriel Forray <gabriel.forray@gmail.com>
2021-02-09 10:18:14 +0200

    Lecture des CSV et décomposition par lignes

---
+++ test.py
@@ -0,0 +1,14 @@
+import csv
+
+def read_csv(fichier):
+	
+	with open(fichier, newline='') as csvfile:
+		reader = csv.reader(csvfile, delimiter = ' ', quotechar='|')
+		data = []
+		for row in reader:
+			data.append(row[0].split(","))
+		return data
+
+for row in read_csv('golf.csv'):
+	print(row)
+
