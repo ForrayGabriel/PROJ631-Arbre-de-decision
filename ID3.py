fbecbe9dcd05796621641e726d9cec340a286604
ForrayGabriel <gabriel.forray@gmail.com>
2021-02-10 20:45:56 +0100

    Changed Entropy calcul for a universal way

--- ID3.py
+++ ID3.py
@@ -4,16 +4,20 @@ import math
 def ID3(fichier):
 
 	Sapp = Sr.get_Sapp(fichier)
-	p("yes", Sapp)
-	entropy(Sapp)
+	entropy_set(Sapp, 4)
+	print(Sapp[0], "\n", Sapp[1], "\n", Sapp[2])
+
+def Information_Gain(S, A):
+	E = entropy_set(S)
+
 
 #Function that calculate the entropy
-def entropy(S):
+def entropy_set(S, index):
 
 	entropy = 0
 
-	for i in range(0,len(S[1][len(S[1])-1])):
-		prob = p(S[1][len(S[1])-1][i],S)
+	for i in range(0,len(S[1][index])):
+		prob = p(S[1][index][i],S, index)
 		entropy -= prob*math.log2(prob)
 
 	print(entropy)
@@ -21,14 +25,14 @@ def entropy(S):
 
 
 #function that return the proportion of "result" in the last column of S
-def p(result, S):
+def p(result, S, index):
 
 	cpt_occurence = 0
 	cpt_total = 0
 
 	for i in S[2]:
 		cpt_total += 1
-		if result == i[len(i)-1]:
+		if result == i[index]:
 			cpt_occurence +=1
 	return cpt_occurence/cpt_total
 
